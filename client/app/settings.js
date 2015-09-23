9f9dfad673125c64b9998053485ddc3cf95f7ea4
joegatt <joe@joegatt.net>
2015-09-23 10:06:52 +0200

    Add constants for colours
    (Not in use yet)

--- client/app/settings.js
+++ client/app/settings.js
@@ -1,2 +1,16 @@
 const HOME_INDEX_LENGTH = 3;
 const NOTES_INDEX_LENGTH = 10;
+
+// Palette (Hex, HSL)
+
+const GREY = '#5A5A66'; // 249, 12, 40
+const BLACK = '#222222'; // 0, 0, 13
+const WHITE = '#FAFAFA'; // 0, 0, 98
+
+const RED = '#E62E2E'; // 0, 80, 90
+const BLUE = '#2E31E6'; // 239, 80, 90
+const CYAN = '#2ED0E6'; // 187, 80, 90
+const YELLOW = '#D3E62E'; // 66, 80, 90
+const PURPLE = '#E62EDF'; // 302, 80, 90
+const GREEN = '#2EE634'; // 122, 80, 90
+const ORANGE = '#E6812E'; // 27, 80, 90
