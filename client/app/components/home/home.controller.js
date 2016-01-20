756877d732a8e40303b96310ec54d33632a27292
joegatt <joe@joegatt.net>
2016-01-20 18:44:22 +0100

    Tidy up

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -2,8 +2,6 @@ class HomeController {
   constructor($http) {
     this.name = 'home';
 
-    // this.xxx = HOME_INDEX_LENGTH;
-
     $http.get('http://joegatt.net/texts/p/1.json?blurb=true')
       .then(response => {
         this.list = response.data.texts.slice(0, 6);
