800d66e6f50a726cb9ab33438a067d6b14270a82
Joe Gatt <joe@joegatt.net>
2017-12-08 20:02:58 +0100

    Add Texts list

--- src/containers/Texts/index.js
+++ src/containers/Texts/index.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class Texts extends Component {
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.key !== nextProps.location.key) {
+      this.props.allNotesQuery.refetch()
+    }
+  }
+
   render() {
+    if (this.props.allNotesQuery.loading) {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
+
+    let blurClass = ''
+    if (this.props.location.pathname !== '/') {
+      blurClass = ' blur'
+    }
+
     return (
-      <div className="Texts">
-        Texts
+      <div>
+        <ul>
+          {this.props.allNotesQuery.allNotes && this.props.allNotesQuery.allNotes.nodes.map(text => (
+            <li><Link to={`/texts/${text.id}`}>{text.title}</Link></li>
+          ))}
+        </ul>
+        {this.props.children}
       </div>
-    );
+    )
   }
 }
 
-export default Texts;
+const ALL_NOTES_QUERY = gql`
+  query AllNotesQuery {
+    allNotes(orderBy: UPDATED_AT_DESC) {
+      nodes {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export default graphql(ALL_NOTES_QUERY, {
+  name: 'allNotesQuery',
+  options: {
+    fetchPolicy: 'network-only',
+  },
+})(Texts);
