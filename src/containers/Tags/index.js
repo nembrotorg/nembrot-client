22bae1608c6218c3d6a632a7481b010a4db7c583
Joe Gatt <joe@joegatt.net>
2018-08-09 13:10:38 +0200

    Use Apollo 2.1

--- src/containers/Tags/index.js
+++ src/containers/Tags/index.js
@@ -1,44 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-
-class Tags extends Component {
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.location.key !== nextProps.location.key) {
-      this.props.allValidTagsQuery.refetch()
-    }
-  }
-
-  render() {
-    const {
-      allValidTagsQuery,
-      children
-    } = this.props;
-
-    if (allValidTagsQuery.loading) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
-
-    return (
-      <div>
-        <ul>
-          {allValidTagsQuery.activeTags && allValidTagsQuery.activeTags.nodes.map((tag, index) => (
-            <li key={index}>
-              <Link to={`/tags/${tag.slug}`}>{tag.name}</Link> <span className="count">{tag.activeTagsCount}</span>
-            </li>
-          ))}
-        </ul>
-        {children}
-      </div>
-    )
-  }
-}
 
 const ALL_VALID_TAGS_QUERY = gql`
   query AllValidTagsQuery {
@@ -53,9 +16,28 @@ const ALL_VALID_TAGS_QUERY = gql`
   }
 `;
 
-export default graphql(ALL_VALID_TAGS_QUERY, {
-  name: 'allValidTagsQuery',
-  options: {
-    fetchPolicy: 'network-only',
-  },
-})(Tags);
+class Tags extends Component {
+
+  render() {
+    return (
+      <Query query={ALL_VALID_TAGS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+          return (
+            <ul>
+              {data.activeTags.nodes.map((tag, index) => (
+                <li key={tag.slug}>
+                  <Link to={`/tags/${tag.slug}`}>{tag.name}</Link>
+                  <span className="count">{tag.activeTagsCount}</span>
+                </li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Tags;
