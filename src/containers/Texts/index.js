746cc2eb8e31e9d01b81d4b5ab82dec8c86913bd
Joe Gatt <joe@joegatt.net>
2018-09-05 18:55:57 +0200

    Add notes list

--- src/containers/Texts/index.js
+++ src/containers/Texts/index.js
@@ -1,62 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-class Texts extends Component {
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.location.key !== nextProps.location.key) {
-      this.props.allNotesQuery.refetch()
-    }
-  }
-
-  render() {
-    const query = this.props.allNotesQuery;
-    if (query.loading) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
-    return (
-      <div>
-        <ul>
-          {query.allNotes && query.allNotes.nodes.map((link, index) => (
-            link.cachedUrl && <li key={index}>
-              <Link to={link.cachedUrl} dangerouslySetInnerHTML={{ __html: link.cachedBlurbHtml }} />
-            </li>
-          ))}
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-const ALL_NOTES_QUERY = gql`
-  query AllNotesQuery {
-    allNotes(
-      condition: {
-        contentType: 0,
-        hide: false,
-        isSection: false,
-        listable: true,
-      },
-      orderBy: UPDATED_AT_DESC
-    ) {
+const ALL_VALID_TEXTS_QUERY = gql`
+  query AllValidTextsQuery {
+    activeNotes {
       nodes {
-        cachedUrl
-        cachedBlurbHtml
+        id
+        title
+        role
       }
     }
   }
 `;
 
-export default graphql(ALL_NOTES_QUERY, {
-  name: 'allNotesQuery',
-  options: {
-    fetchPolicy: 'network-only',
-  },
-})(Texts);
+class Texts extends Component {
+
+  render() {
+    return (
+      <Query query={ALL_VALID_TEXTS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
+          return (
+            <ul>
+              {data.activeNotes.nodes.map((text, index) => (
+                <li key={text.id}>
+                  <Link to={`/texts/${text.id}`}>{text.title} | {text.role}</Link>
+                </li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Texts;
